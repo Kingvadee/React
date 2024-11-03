12c1a5e02bdb6ca4934225031638d460d965d1b6
Kingvadee <ojeifod8@gmail.com>
2024-11-03 16:01:21 +0100

    Updated linktree URL

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           This is your number 1 platform for everything exciting!
           From learning how to monetize your time on the internet to freelancing and much more!
           Stick around and follow me on all social media platforms where I will be dropping helpful content.
-          <a href="https://linktr.ee/davido" target="_blank" rel="noopener noreferrer">Linktree</a>
+          <a href="https://linktr.ee/tonofgem" target="_blank" rel="noopener noreferrer">Linktree</a>
         </p>
         <TwitterTimelineEmbed
-          sourceType="profile"
-          screenName="yourTwitterHandle"
+          sourceType="https://x.com/tonOfGem"
+          screenName="tonOfGem"
           options={{ height: 400 }}
         />
         <Blog />
